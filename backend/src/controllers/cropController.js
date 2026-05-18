ccdfcbb1ff344ea9858dd12168a72a77b0d2d21e
Skywolf02 <henilchaudhari4422@gmail.com>
2026-05-18 23:10:11 +0530

    save crop quantity in controller

--- backend/src/controllers/cropController.js
+++ backend/src/controllers/cropController.js
@@ -1,23 +1,23 @@
-import Crop from '../models/Crop.js';
+import Crop from "../models/Crop.js";
 
 // CREATE
 export const createCrop = async (req, res) => {
   try {
-    const { title, price, location, description } = req.body;
+    const { title, price, quantity, location, description } = req.body;
 
     const image = req.file ? req.file.filename : null;
 
     const crop = await Crop.create({
       title,
       price,
+      quantity,
       location,
       description,
       image,
-      user: req.user._id
+      user: req.user._id,
     });
 
     res.status(201).json(crop);
-
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -39,12 +39,12 @@ export const deleteCrop = async (req, res) => {
     const crop = await Crop.findById(req.params.id);
 
     if (!crop) {
-      return res.status(404).json({ message: 'Crop not found' });
+      return res.status(404).json({ message: "Crop not found" });
     }
 
     await crop.deleteOne();
-    res.json({ message: 'Crop deleted' });
 
+    res.json({ message: "Crop deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -56,17 +56,18 @@ export const updateCrop = async (req, res) => {
     const crop = await Crop.findById(req.params.id);
 
     if (!crop) {
-      return res.status(404).json({ message: 'Crop not found' });
+      return res.status(404).json({ message: "Crop not found" });
     }
 
     crop.title = req.body.title || crop.title;
     crop.price = req.body.price || crop.price;
+    crop.quantity = req.body.quantity || crop.quantity;
     crop.location = req.body.location || crop.location;
     crop.description = req.body.description || crop.description;
 
     const updated = await crop.save();
-    res.json(updated);
 
+    res.json(updated);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
