b127aaad77f2b49cd041dcdb8771f48418944b75
Skywolf02 <henilchaudhari4422@gmail.com>
2026-05-18 17:16:11 +0530

    add mobile hamburger menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const closeMenu = () => setOpen(false);
+
   return (
     <header className="navbar">
       <div className="container navbar-inner">
-        <Link to="/" className="brand">
+        <Link to="/" className="brand" onClick={closeMenu}>
           <span className="brand-icon">🌱</span>
           <span className="brand-text">CropCircle</span>
-          <span style={{ fontSize: "0.8rem", color: "#5c6b5d" }}>
-  Smart crop marketplace
-</span>
+          <span className="brand-tagline">Smart crop marketplace</span>
         </Link>
 
-        <nav className="nav-links">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/marketplace">Marketplace</NavLink>
-          <NavLink to="/sell-crop">Sell Crop</NavLink>
-          <NavLink to="/crop-info">Crop Info</NavLink>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/signup">Signup</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+        <button className="menu-toggle" onClick={() => setOpen(!open)}>
+          ☰
+        </button>
+
+        <nav className={`nav-links ${open ? "show-menu" : ""}`}>
+          <NavLink to="/" onClick={closeMenu}>Home</NavLink>
+          <NavLink to="/marketplace" onClick={closeMenu}>Marketplace</NavLink>
+          <NavLink to="/sell-crop" onClick={closeMenu}>Sell Crop</NavLink>
+          <NavLink to="/crop-info" onClick={closeMenu}>Crop Info</NavLink>
+          <NavLink to="/login" onClick={closeMenu}>Login</NavLink>
+          <NavLink to="/signup" onClick={closeMenu}>Signup</NavLink>
+          <NavLink to="/contact" onClick={closeMenu}>Contact</NavLink>
         </nav>
       </div>
     </header>
